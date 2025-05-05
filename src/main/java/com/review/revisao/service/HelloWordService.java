7c0911cf7999201e7a969f92ccbf23001f4d9596
bispobr <156684382+bispobr@users.noreply.github.com>
2025-05-05 15:21:59 -0300

    feat: adiciona logs

--- src/main/java/com/review/revisao/service/HelloWordService.java
+++ src/main/java/com/review/revisao/service/HelloWordService.java
@@ -1,10 +1,13 @@
 package com.review.revisao.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class HelloWordService {
     public String HelloWord(String nome){
+        log.info("nome retornado com sucesso");
         return "Hello! " + nome;
     }
 }
