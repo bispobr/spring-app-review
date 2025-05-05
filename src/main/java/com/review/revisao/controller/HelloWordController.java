27e9cbd722cdfadde0e3ef3838eea905546047e3
bispobr <156684382+bispobr@users.noreply.github.com>
2025-05-05 15:36:26 -0300

    feat: adiciona logs e documentação com swagger

--- src/main/java/com/review/revisao/controller/HelloWordController.java
+++ src/main/java/com/review/revisao/controller/HelloWordController.java
@@ -1,10 +1,16 @@
 package com.review.revisao.controller;
 
 import com.review.revisao.domain.User;
+import com.review.revisao.dto.UserDto;
 import com.review.revisao.service.HelloWordService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/hello-word")
 public class HelloWordController {
@@ -13,12 +19,22 @@ public class HelloWordController {
     private HelloWordService helloWordService;
 
     @GetMapping
+    @Operation(description = "EndPoint responsável por retorna o nome usuário")
+    @ApiResponse (responseCode = "200", description = "Solicitação Retornado com Sucesso")
+    @ApiResponse(responseCode = "400",description = "Erro de requisição")
+    @ApiResponse(responseCode = "500",description = "Erro Interno")
     public String helloWord(){
+        log.info("Solicitação helloword recebida");
         return helloWordService.HelloWord("Usuario");
     }
 
     @PostMapping
-    public String helloWordPost(@RequestBody User body){
-        return "Hello Word " + body.getNome();
+    @Operation(description = "EndPoint responsável por retorna o nome de usuário passado por parâmetro ")
+    @ApiResponse (responseCode = "200", description = "Solicitação Retornado com Sucesso")
+    @ApiResponse(responseCode = "400",description = "Erro de requisição")
+    @ApiResponse(responseCode = "500",description = "Erro Interno")
+    public String helloWordPost(@Valid @RequestBody UserDto body){
+        log.info("solicitação helloWordPost recebida nome: " + body.nome());
+        return "Hello Word " + body.nome();
     }
 }
